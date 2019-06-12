e0c5f4b7041702103a5551caf749042f5d2d97ec
Daniel Felizardo <daniel.david772@gmail.com>
2019-06-12 18:00:35 -0300

    finalizando aplicação do instagram copy

---
+++ src/pages/New.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+
+import api from "../services/api";
+
+import "./New.css";
+
+class New extends Component {
+  state = {
+    image: null,
+    author: "",
+    place: "",
+    description: "",
+    hashtags: ""
+  };
+
+  handleSubmit = async e => {
+    const { image, author, place, description, hashtags } = this.state;
+    e.preventDefault();
+
+    const data = new FormData();
+
+    data.append("image", image);
+    data.append("author", author);
+    data.append("place", place);
+    data.append("description", description);
+    data.append("hashtags", hashtags);
+
+    await api.post("posts", data);
+
+    this.props.history.push("/");
+  };
+
+  handleImageChange = e => {
+    this.setState({ image: e.target.files[0] });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { author, place, description, hashtags } = this.state;
+    return (
+      <form id="new-post" onSubmit={this.handleSubmit}>
+        <input type="file" onChange={this.handleImageChange} />
+        <input
+          type="text"
+          name="author"
+          placeholder="Autor do Post"
+          onChange={this.handleChange}
+          value={author}
+        />
+        <input
+          type="text"
+          name="place"
+          placeholder="Local do Post"
+          onChange={this.handleChange}
+          value={place}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Descrição do Post"
+          onChange={this.handleChange}
+          value={description}
+        />
+        <input
+          type="text"
+          name="hashtags"
+          placeholder="Hashtags do Post"
+          onChange={this.handleChange}
+          value={hashtags}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default New;
